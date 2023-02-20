9e222d8b646a4e876c872f66482c7efa91c3e77e
Prabhakaran <lttespk@gmail.com>
2023-02-20 11:39:16 +0530

    Simple App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formArray';
+
+  ipAddressForm: FormGroup;
+  IPADDR = [1,2,3];
+
+  get ipAddr() {
+    return this.ipAddressForm.get('ipAddrFrm') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.ipAddressForm = this.fb.group({
+      ipAddrFrm: this.fb.array([this.fb.control('')])
+    })
+  }
+
+  newIpAddr() {
+    this.ipAddr.push(this.fb.control(''));
+  }
+
+  submit() {
+    console.log(this.ipAddressForm.getRawValue());
+  }
+
+  patchValue() {
+    this.IPADDR.forEach(a => {
+      this.ipAddr.push(new FormControl(a));
+    })
+  }
+
+  deleteIp(index: number) {
+    this.ipAddr.removeAt(index);
+  }
 }
